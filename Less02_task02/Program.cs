34b444b6c34763a7cef20a26d4cbb6b6af0d7d77
Hayk <hayk.hayr3@gmail.com>
2020-12-04 01:24:53 +0400

    Add project files.

---
+++ Less02_task02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Less02_task02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass inst = new MyClass();
+			Console.WriteLine(inst.Field);
+
+			Console.WriteLine(inst.Field);
+
+			Console.ReadKey();
+		}
+
+		class MyClass
+		{
+			readonly string field = "Field0";
+
+			public string Field { get { return field; } }
+			public MyClass()
+			{
+				field = "Field1";
+			}
+		}
+	}
+}
